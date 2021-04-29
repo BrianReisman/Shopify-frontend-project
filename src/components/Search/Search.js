5d494b973695120fc5f5daa42039b7f90655af7e
BrianReisman <brianreisman@protonmail.com>
2021-04-29 15:07:45 -0400

    API call working

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,18 +1,35 @@
-import React from "react";
-import SearchRoot, { Form, Input, Button } from "./SearchElements";
+import React, { useState } from "react";
+import { Form, Input, Button } from "./SearchElements";
 import { ViewHeader, ViewRoot } from "../../GlobalComponents";
+import axios from "axios";
 
 const Search = (props) => {
+  const [input, setInput] = useState("");
+
+  const changeHandler = (e) => {
+    setInput(e.target.value);
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
-    alert("submitted!");
+    // console.log(process.env);
+    axios
+      .get(`http://www.omdbapi.com/?apikey=c951a210&t=${input}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    setInput("");
   };
 
   return (
     <ViewRoot>
       <ViewHeader>dsf</ViewHeader>
       <Form onSubmit={submitHandler}>
-        <Input type="search" />
+        <Input type="search" value={input} onChange={changeHandler} />
         <Button>Search</Button>
       </Form>
     </ViewRoot>
